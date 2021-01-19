76443716cb358d9d1121243e5cc13f253937f3f4
dkessler <dkessler@pillartechnology.com>
2021-01-19 14:04:45 -0600

    reformatted code

--- src/main/java/com/pairgood/GildedRose.java
+++ src/main/java/com/pairgood/GildedRose.java
@@ -24,8 +24,7 @@ public class GildedRose {
                 }
             }
 
-            if (name.equals("Aged Brie")
-                    || name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (name.equals("Aged Brie") || name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 quality = increaseQuality(quality);
             } else {
                 if (quality > 0) {
